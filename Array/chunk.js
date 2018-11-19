9b3e405c691b2ed6609f0ea9345fba4a870f7336
wungjyan <1170017412@qq.com>
2018-11-19 15:12:22 +0800

    获取数组某个数值出现的个数

---
+++ Array/chunk.js
@@ -0,0 +1,13 @@
+// 把一个数组分块成指定大小的小数组。
+
+const chunk = (arr, size) => {
+  // 新分组数组的长度
+  const len = Math.ceil(arr.length / size);
+
+  // 使用Array.form生成新数组(可参考阮一峰es6)
+  return Array.from({ length: len }, (v, i) => {
+    return arr.slice(i * size, i * size + size);
+  });
+};
+
+console.log(chunk([1, 2, 3, 4, 5, 6], 2));
